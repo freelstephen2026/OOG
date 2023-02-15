ef71eeab434e3d0d59fbe5bae7632628c2cff70f
freelstephen2026 <freelstephen3@gmail.com>
2023-02-15 17:55:44 -0500

    New commit

--- git.sh
+++ git.sh
@@ -6,7 +6,7 @@ git add .
 git commit -m "New commit"
 num=11
 printf "argc = %d" $#
-if [ argc -lt 1 ]; then
+if [ $argc -lt 1 ]; then
     echo hifdsafdsafahgtjywjywwkyawsk6yasjk6ysj6ysjkysrjykysrysjy6ts
 fi
 git remote add origin https://github.com/freelstephen2026/OOG.git
