6c24fd29eedf31b4551f8d1f976ba820fd3d5c28
freelstephen2026 <freelstephen3@gmail.com>
2023-02-15 17:46:32 -0500

    New commit

--- git.sh
+++ git.sh
@@ -1,6 +1,9 @@
+# note: `$#` is argc, $1 is arg 1, $2 is arg 2, and so on.
 git pull
 git init
 git add .
 git commit -m "New commit"
+if ($# > 1)
+    echo hi
 git remote add origin https://github.com/freelstephen2026/OOG.git
 git push origin master
