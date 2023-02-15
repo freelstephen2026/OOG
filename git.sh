44f380dbc4502d6d5740a970f55b780672cf6d63
freelstephen2026 <freelstephen3@gmail.com>
2023-02-15 17:51:29 -0500

    New commit

--- git.sh
+++ git.sh
@@ -4,7 +4,7 @@ git init
 git add .
 git commit -m "New commit"
 num=11
-if [ 2 -gt 1 ]; then echo 3
+if [ $1 -gt 1 ]; then echo 9999999999999999999999999
 fi
 git remote add origin https://github.com/freelstephen2026/OOG.git
 git push origin master
