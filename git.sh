556f9599bb060d92659572b38d3482b67c2229c0
freelstephen2026 <freelstephen3@gmail.com>
2023-02-15 17:56:43 -0500

    New commit

--- git.sh
+++ git.sh
@@ -1,12 +1,13 @@
 # note: `$#` is argc - 1, $1 is arg 1, $2 is arg 2, and so on.
 argc=$# - 1
+num=1
 git pull
 git init
 git add .
 git commit -m "New commit"
 num=11
 printf "argc = %d" $#
-if [ 1 -gt $argc ]; then
+if [ $num -gt $argc ]; then
     echo hifdsafdsafahgtjywjywwkyawsk6yasjk6ysj6ysjkysrjykysrysjy6ts
 fi
 git remote add origin https://github.com/freelstephen2026/OOG.git
